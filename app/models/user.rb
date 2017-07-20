b4a2fec78ebc7e781cc6a2b015a8ca00d00b5df3
Fane <fane247@gmail.com>
2017-07-20 16:42:25 +0100

    got chat working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_and_belongs_to_many :events
+  has_many :conversations
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
